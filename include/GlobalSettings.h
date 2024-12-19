b174c6773e7b3bbc35644f9ccd76f0299e846769
huan27042003 <huan27042003@gmail.com>
2024-12-19 14:46:03 +0700

    Update motor pin

--- include/GlobalSettings.h
+++ include/GlobalSettings.h
@@ -20,20 +20,20 @@ extern double kiPos;
 extern double kdPos;
 
 extern int dir;
-
+extern String message;
 // Pin configurations for motors
+extern motorPin motorPin0; // pinA, pinB, L_PWM, R_PWM
 extern motorPin motorPin1; // pinA, pinB, L_PWM, R_PWM
 extern motorPin motorPin2; // pinA, pinB, L_PWM, R_PWM
 extern motorPin motorPin3; // pinA, pinB, L_PWM, R_PWM
-extern motorPin motorPin4; // pinA, pinB, L_PWM, R_PWM
 
 // Wheel objects
 extern Wheel wheels[];
 
 // Declare the trigger interrupt function
+extern void triggerW0();
 extern void triggerW1();
 extern void triggerW2();
 extern void triggerW3();
-extern void triggerW4();
 
 #endif // !__GLOBALSETTINGS_H__
--- src/GlobalSettings.cpp
+++ src/GlobalSettings.cpp
@@ -14,20 +14,21 @@ double kdPos = 0;
 
 int dir = 1;
 
+String message = "";
 // Pin configurations for motors
-motorPin motorPin1 = motorPin(4, 5, 15, 2);    // pinA, pinB, L_PWM, R_PWM
+motorPin motorPin0 = motorPin(4, 5, 15, 2);    // pinA, pinB, L_PWM, R_PWM
+motorPin motorPin1 = motorPin(27, 26, 25, 33); // pinA, pinB, L_PWM, R_PWM
 motorPin motorPin2 = motorPin(21, 22, 18, 19); // pinA, pinB, L_PWM, R_PWM
-motorPin motorPin3 = motorPin(27, 26, 13, 14); // pinA, pinB, L_PWM, R_PWM
-motorPin motorPin4 = motorPin(32, 35, 25, 33); // pinA, pinB, L_PWM, R_PWM
+motorPin motorPin3 = motorPin(32, 35, 13, 14); // pinA, pinB, L_PWM, R_PWM
 
 // Declare the trigger interrupt function
 Wheel wheels[] = {
-    Wheel(motorPin1.pinA, motorPin1.pinB, motorPin1.L_PWM, motorPin1.R_PWM),
-    Wheel(motorPin2.pinA, motorPin2.pinB, motorPin2.L_PWM, motorPin2.R_PWM),
-    Wheel(motorPin3.pinA, motorPin3.pinB, motorPin3.L_PWM, motorPin3.R_PWM),
-    Wheel(motorPin4.pinA, motorPin4.pinB, motorPin4.L_PWM, motorPin4.R_PWM)};
+    Wheel(0, motorPin0.pinA, motorPin0.pinB, motorPin0.L_PWM, motorPin0.R_PWM),
+    Wheel(1, motorPin1.pinA, motorPin1.pinB, motorPin1.L_PWM, motorPin1.R_PWM),
+    Wheel(2, motorPin2.pinA, motorPin2.pinB, motorPin2.L_PWM, motorPin2.R_PWM),
+    Wheel(3, motorPin3.pinA, motorPin3.pinB, motorPin3.L_PWM, motorPin3.R_PWM)};
 
-void triggerW1() { wheels[0].triggerInterrupt(); };
-void triggerW2() { wheels[1].triggerInterrupt(); };
-void triggerW3() { wheels[2].triggerInterrupt(); };
-void triggerW4() { wheels[3].triggerInterrupt(); };
+void triggerW0() { wheels[0].triggerInterrupt(); };
+void triggerW1() { wheels[1].triggerInterrupt(); };
+void triggerW2() { wheels[2].triggerInterrupt(); };
+void triggerW3() { wheels[3].triggerInterrupt(); };
