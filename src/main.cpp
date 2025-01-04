2d0327dd9fcc0d12ebfdde6f90c249d4a050112b
huan27042003 <huan27042003@gmail.com>
2025-01-04 13:21:26 +0700

    Test pid position

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <WiFi.h>
 #include <PID_v1.h>
 #include <ArduinoJson.h>
+#include <queue>
 
 #define LED 2
 
@@ -22,8 +23,8 @@ inline const char *password = "YOUR_PASSWORD"; // Replace with your network cred
 #endif// Replace with your network credentials
 
 #define UART_BUFFER_SIZE 256
-#define VELOCITY 0
-// #define POSITION 1
+// #define VELOCITY 0
+#define POSITION 1
 
 HardwareSerial camSerial(2);
 uint8_t buffer[UART_BUFFER_SIZE];
@@ -34,7 +35,16 @@ JsonDocument doc;
 long previousMillis = 0;
 long currentMillis = 0;
 
+int interval_pid_position = 1000;
+
+long previousMillisInfoPosition = 0;
+
+int positions[] = {0, 0, 0, 0};
+// int SZ = 3;
+// int curSZ = 0;
+
 long previousMillisInfoVelocity = 0;
+long previousMillisPIDPosition = 0;
 
 const int port = 8080;
 const int stopSignal = 20;
@@ -116,34 +126,34 @@ void setup()
 #endif // POSITION
 
 
-  // TODO: Testing PID plot
-    // Create the task for sending data, pinned to Core 1
-  xTaskCreatePinnedToCore(
-    sendDataTask,    // Task function
-    "SendDataTask",  // Name of the task
-    10000,           // Stack size (in words)
-    NULL,            // Task parameter
-    10,               // Priority (higher value = higher priority)5
-    &sendDataTaskHandle, // Task handle
-    1                // Core to run the task (0 = Core 0, 1 = Core 1)
-  );
+  // // TODO: Testing PID plot
+  //   // Create the task for sending data, pinned to Core 1
+  // xTaskCreatePinnedToCore(
+  //   sendDataTask,    // Task function
+  //   "SendDataTask",  // Name of the task
+  //   10000,           // Stack size (in words)
+  //   NULL,            // Task parameter
+  //   10,               // Priority (higher value = higher priority)5
+  //   &sendDataTaskHandle, // Task handle
+  //   1                // Core to run the task (0 = Core 0, 1 = Core 1)
+  // );
 
-  // TODO: ???
-  WiFi.begin(ssid, password);
+  // // TODO: ???
+  // WiFi.begin(ssid, password);
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
+  // // Wait for connection
+  // while (WiFi.status() != WL_CONNECTED)
+  // {
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
 
-  Serial.println("Connected to WiFi!");
-  Serial.print("ESP32 IP Address: ");
-  Serial.println(WiFi.localIP());
+  // Serial.println("Connected to WiFi!");
+  // Serial.print("ESP32 IP Address: ");
+  // Serial.println(WiFi.localIP());
 
-  // Start the serverd
-  server.begin();
+  // // Start the serverd
+  // server.begin();
 
 }
 
@@ -204,7 +214,7 @@ void loop()
   if (currentMillis - previousMillis > interval_position)
   {
     for (int i = 0; i < WHEEL_COUNT; i++) {
-      wheels[i].infoPosition();
+      // wheels[i].infoPosition();
       wheels[i].tuningPosition();
       if (wheels[i].getDirection() == LEFT_DIR) {
         wheels[i].getMotor().TurnLeft(wheels[i].getPWM());
@@ -214,6 +224,30 @@ void loop()
     }
     previousMillis = currentMillis;
   }
+
+  if (currentMillis - previousMillisInfoPosition > interval_pid_position)
+  {
+    for (int i = 0; i < WHEEL_COUNT; i++)
+    {
+      wheels[i].infoPosition();
+    }
+    previousMillisInfoPosition = currentMillis;
+  }
+
+  if (currentMillis - previousMillisPIDPosition > interval_pid_position)
+  {
+    // if(curSZ < SZ) {
+      for (int i = 0; i < WHEEL_COUNT; i++)
+      {
+        // positions[cur] += 130;
+        wheels[i].setTargetPosition(positions[i]);
+      }
+      // curSZ += 1;
+    // }
+    previousMillisPIDPosition = currentMillis;
+  }
+
+  SerialDataWrite();
 
 #endif // POSITION
 
@@ -285,8 +319,14 @@ void SerialDataWrite()
     {
       // Serial.println(received_chars);
       int option = received_chars.toInt();
-      char c = received_chars[0];
-      command_test(c);
+      Serial.print("Add position: ");
+      Serial.println(option);
+      positions[0] += option;
+      positions[1] -= option;
+      positions[2] += option;
+      positions[3] -= option;
+      // char c = received_chars[0];
+      // command_test(c);
       received_chars = "";
     }
   }
@@ -294,55 +334,71 @@ void SerialDataWrite()
 double velo_test = 0.20;
 double velo_rotate = 0.25;
 
+// void command_test(char option){
+//   Serial.print("Change movement: ");
+//   Serial.println(option);
+//   switch (option)
+//   {
+//   case '1':
+//     mecanumCar.move(-velo_test, velo_test, 0.0);
+//     break;
+//   case '2':
+//     mecanumCar.move(-velo_test, 0, 0.0);
+//     break;
+//   case '3':
+//     mecanumCar.move(-velo_test, -velo_test, 0.0);
+//     break;
+//   case '4':
+//     mecanumCar.move(0, velo_test, 0.0);
+//     break;
+//   case '5':
+//     mecanumCar.move(0.0, 0.0, 0.0);
+//     Serial.println("Stopped");
+//     break;
+//   case '6':
+//     mecanumCar.move(0, -velo_test, 0.0);
+//     break;
+//   case '7':
+//     mecanumCar.move(velo_test, velo_test, 0.0);
+//     break;
+//   case '8':
+//     mecanumCar.move(velo_test, 0, 0.0);
+//     break;
+//   case '9':
+//     mecanumCar.move(velo_test, -velo_test, 0.0);
+//     break;
+//   case 'p': //rotate right
+//     mecanumCar.move(0, 0, -velo_rotate);
+//     break;
+//   case 'o': //rotate left
+//     mecanumCar.move(0, 0, velo_rotate);
+//     break;
+//   case 'k': //drift left
+//     mecanumCar.move(0.0, velo_test, velo_rotate);
+//     break;
+//   case 'l': //drift rightp
+//     mecanumCar.move(0.0, -velo_test, -velo_rotate);
+//     break;
+//   default:
+//     mecanumCar.move(0, 0, 0);
+//     Serial.println("Error");
+//     break;
+//   }
+// }
+
+
 void command_test(char option){
   Serial.print("Change movement: ");
   Serial.println(option);
   switch (option)
   {
   case '1':
-    mecanumCar.move(-velo_test, velo_test, 0.0);
-    break;
-  case '2':
-    mecanumCar.move(-velo_test, 0, 0.0);
-    break;
-  case '3':
-    mecanumCar.move(-velo_test, -velo_test, 0.0);
-    break;
-  case '4':
-    mecanumCar.move(0, velo_test, 0.0);
-    break;
-  case '5':
-    mecanumCar.move(0.0, 0.0, 0.0);
-    Serial.println("Stopped");
-    break;
-  case '6':
-    mecanumCar.move(0, -velo_test, 0.0);
-    break;
-  case '7':
-    mecanumCar.move(velo_test, velo_test, 0.0);
-    break;
-  case '8':
-    mecanumCar.move(velo_test, 0, 0.0);
-    break;
-  case '9':
-    mecanumCar.move(velo_test, -velo_test, 0.0);
-    break;
-  case 'p': //rotate right
-    mecanumCar.move(0, 0, -velo_rotate);
-    break;
-  case 'o': //rotate left
-    mecanumCar.move(0, 0, velo_rotate);
-    break;
-  case 'k': //drift left
-    mecanumCar.move(0.0, velo_test, velo_rotate);
-    break;
-  case 'l': //drift rightp
-    mecanumCar.move(0.0, -velo_test, -velo_rotate);
+    
     break;
   default:
-    mecanumCar.move(0, 0, 0);
     Serial.println("Error");
     break;
   }
 }
+
 
